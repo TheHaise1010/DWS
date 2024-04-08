fd3f5370d262b0cc34f531d618b9310829830a3f
JordanZ <jordan.zelayahs@gmail.com>
2024-04-08 11:17:11 -0600

    Register form added

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -8,11 +8,13 @@ import {FormsModule} from "@angular/forms";
 import {HttpClientModule} from "@angular/common/http";
 import {ReactiveFormsModule} from "@angular/forms";
 import { NavComponent } from './nav/nav.component';
+import { RegisterComponent } from './register/register.component';
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     NavComponent,
+    RegisterComponent,
   ],
   imports: [
     BrowserModule,
